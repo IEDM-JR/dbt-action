ad8ac6bfb7016c3e96db98e5596977f3710b4196
chengzilin2011@gmail.com <chengzilin2011@gmail.com>
2022-11-23 04:46:45 +0000

    update on mart file and dbt_project.yml

--- models/marts/mart_product_times.sql
+++ models/marts/mart_product_times.sql
@@ -1,4 +1,6 @@
-select Product_id, count(*) From {{ref('stg_order_product')}} as purchased_times
+select b.Product_name as product_name, count(a.*) as purchased_times
+From {{ref('stg_order_product')}} a join {{ref('stg_products')}} b 
+on a.product_id=b.product_id
 group by 1 
 order by 2 desc limit 10
 
--- models/order_condition.sql
+++
@@ -1,6 +0,0 @@
-SELECT DISTINCT user_id , 
-SUM(order_hour_of_day) AS use_hours, SUM(days_since_prior_order) AS gap_days
-FROM {{ source ('IEDM','ORDERS')}}
-GROUP BY user_id
-ORDER BY use_hours DESC
-LIMIT 40
