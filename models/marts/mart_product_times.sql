8a2eeea92992f2f627787481d6738dc2ad8f84a4
chengzilin2011@gmail.com <chengzilin2011@gmail.com>
2022-11-30 11:48:43 +0000

    remove one mart file

--- models/marts/mart_product_times.sql
+++
@@ -1,7 +0,0 @@
-select b.Product_name as product_name, count(a.*) as purchased_times
-From {{ref('stg_order_product')}} a join {{ref('stg_products')}} b 
-on a.product_id=b.product_id
-group by 1 
-order by 2 desc limit 10
-
---top 10 best sell product ( by id)
